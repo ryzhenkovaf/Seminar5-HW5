474190b9779523bb1f1bbc9cc3053e753c94013c
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-25 19:45:31 +0700

    add 038

---
+++ HW38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив целых чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+
+Console.Clear();
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int[] startArray = GetRandomArray(10, 10, 100);
+
+int FindDiff (int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    int dif = max - min;
+    return dif;
+}
+PrintArray(startArray);
+Console.WriteLine($"Разница между числами: {FindDiff(startArray)}");
+
+void PrintArray(int[] startArray)
+{
+    for(int i = 0; i < startArray.Length; i++)
+    {
+        Console.Write(startArray[i] + " ");
+    }
+    Console.WriteLine();
+}
