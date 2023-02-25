d346b7ad266ad1ad2ac07d5b010a191465ffe379
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-25 19:44:01 +0700

    add 031

---
+++ Семинар031/Program.cs
@@ -0,0 +1,27 @@
+﻿// //Задайте массив из 12 элементов, заполненный случайными 
+// числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int[] array = GetRandomArray(12, -9, 9);
+
+int[] SumPosNeg(int[] array)
+{
+    int[] result = new int[2];
+    foreach(int el in array)
+    {
+        result[0] += el > 0 ? el : 0;
+        result[1] += el < 0 ? el : 0;
+    }
+    return result;
+}
+Console.WriteLine($"Positive sum = {SumPosNeg(array)[0]}, Negative sum = {SumPosNeg(array)[1]}");
+Console.WriteLine(String.Join(", ", array));
