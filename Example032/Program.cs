fa29185aad190732be162d0bb41d507566b6a1c4
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-25 19:43:07 +0700

    add 032

---
+++ Example032/Program.cs
@@ -0,0 +1,31 @@
+﻿// /* **Задача 32:**Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int[] startArray = GetRandomArray(6, -10, 10);
+
+int[] InversArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine(String.Join(" ", InversArray(startArray)));
